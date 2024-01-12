6186d93e8178b4e37c73dd9067d0efc42cf1aa81
Jeremy Fuksa <hello@orangefla.me>
2024-01-12 17:38:17 -0600

    Added fresh install option on every action.

--- install.sh
+++ install.sh
@@ -2,6 +2,16 @@
 
 echo "Setting up your environment..."
 
+# Function to prompt user for installation
+prompt_install() {
+		read -p "Do you want to install a new copy? (y/n): " choice
+		case "$choice" in
+				y|Y ) return 0;; # User wants to install
+				n|N ) return 1;; # User does not want to install
+				* ) return 2;;   # Invalid choice
+		esac
+}
+
 # Check if zsh is already the default shell
 if [ "$(basename "$SHELL")" != "zsh" ]; then
 		echo "Changing default shell to Zsh..."
@@ -11,6 +21,7 @@ else
 		echo "Zsh is already the default shell."
 fi
 
+# Check and create ~/.config directory
 if [ ! -d "$HOME/.config" ]; then
 		echo "Creating ~/.config directory..."
 		mkdir "$HOME/.config"
@@ -18,46 +29,51 @@ else
 		echo "~/.config directory already exists."
 fi
 
-# Check if Antigen is installed
+# Check and install Antigen
 if ! [ -f "$HOME/.config/antigen.zsh" ]; then
 		echo "Installing Antigen (Zsh plugin manager)..."
-		curl -L git.io/antigen > $HOME/.config/antigen.zsh
+		curl -L git.io/antigen > "$HOME/.config/antigen.zsh"
 else
 		echo "Antigen is already installed."
+		prompt_install || exit 0
 fi
 
-# Check if Starship is installed
+# Check and install Starship
 if ! command -v starship &> /dev/null; then
 		echo "Installing Starship (Shell prompt)..."
 		sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --yes
 else
 		echo "Starship is already installed."
+		prompt_install || exit 0
 fi
 
-# Check if Starship configuration file exists
+# Check and download Starship configuration
 if [ ! -f "$HOME/.config/starship.toml" ]; then
-	echo "Downloading and saving Starship configuration..."
-	curl -o "$HOME/.config/starship.toml" https://raw.githubusercontent.com/jeremyfuksa/standard-pi-setup/main/starship.toml
+		echo "Downloading and saving Starship configuration..."
+		curl -o "$HOME/.config/starship.toml" https://raw.githubusercontent.com/jeremyfuksa/standard-pi-setup/main/starship.toml
 else
-	echo "Starship configuration file already exists."
+		echo "Starship configuration file already exists."
+		prompt_install || exit 0
 fi
 
-# Check if .zshrc exists
+# Check and download .zshrc
 if ! [ -f "$HOME/.zshrc" ]; then
-	echo "Downloading and saving .zshrc..."
-	curl -o "$HOME/.zshrc" https://raw.githubusercontent.com/jeremyfuksa/standard-pi-setup/main/.zshrc
-	source ~/.zshrc
+		echo "Downloading and saving .zshrc..."
+		curl -o "$HOME/.zshrc" https://raw.githubusercontent.com/jeremyfuksa/standard-pi-setup/main/.zshrc
+		source "$HOME/.zshrc"
 else
-	echo ".zshrc file already exists."
+		echo ".zshrc file already exists."
+		prompt_install || exit 0
 fi
 
-# Check if .zprofile exists
+# Check and download .zprofile
 if ! [ -f "$HOME/.zprofile" ]; then
 		echo "Downloading and saving .zprofile..."
 		curl -o "$HOME/.zprofile" https://raw.githubusercontent.com/jeremyfuksa/standard-pi-setup/main/.zprofile
-		source ~/.zprofile
+		source "$HOME/.zprofile"
 else
 		echo ".zprofile file already exists."
+		prompt_install || exit 0
 fi
 
 echo "Environment setup completed."
